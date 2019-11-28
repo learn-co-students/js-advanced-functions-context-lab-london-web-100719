c4c283a4e1c516901335176a1aa1c6be32941d35
Liko <li_xie1992@hotmail.com>
2019-11-28 10:57:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
-/* Your Code Here */
+const createEmployeeRecord = (array) => {
+    return {
+        firstName: array[0], 
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+const createEmployeeRecords = (recordArray) => {
+    return recordArray.map(record => createEmployeeRecord(record));
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+const createTimeInEvent = function(datestamp) {
+    const [date, time] = datestamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time),
+        date, 
+    });
+    return this;
+}
+
+const createTimeOutEvent = function(datestamp) {
+    const [date, time] = datestamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time),
+        date, 
+    });
+    return this;
+}
+
+const hoursWorkedOnDate = function(date) {
+    const timeIn = this.timeInEvents.find(recordDate => recordDate.date === date).hour;
+    const timeOut = this.timeOutEvents.find(recordDate => recordDate.date === date).hour;
+    
+    return (timeOut - timeIn) / 100;
+}
+
+const wagesEarnedOnDate = function(datestamp) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, datestamp);
+}
 
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+    let dates = this.timeInEvents.map(event => event.date);
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    return dates.reduce((wageTotal, date) => {
+        return wageTotal + wagesEarnedOnDate.call(this, date)
+    }, 0)
+}
 
-    return payable
+const findEmployeeByFirstName = (recordArray, firstName) => {
+    return recordArray.find(record => record.firstName == firstName);
+}
+
+const calculatePayroll = (recordArray) => {
+    return recordArray.reduce((sum, record) => allWagesFor.call(record) + sum, 0 );
 }
